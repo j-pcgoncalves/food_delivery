80d2f6bfcfd542eef479bb072cc9abf7f75c1930
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-03-16 22:07:09 +0000

    add navbar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 50),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: SvgPicture.asset(
+                    'assets/icons/backward.svg',
+                    height: 11,
+                  ),
+                ),
+                SvgPicture.asset(
+                  'assets/icons/menu.svg',
+                  height: 11,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
