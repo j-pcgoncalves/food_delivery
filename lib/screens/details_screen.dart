bb639be1e60a1b042d865fc8e633d366148d0532
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-03-16 22:21:23 +0000

    add footer

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_delivery/constants.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -28,6 +29,94 @@ class DetailsScreen extends StatelessWidget {
                   height: 11,
                 ),
               ],
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 30),
+              padding: const EdgeInsets.all(6),
+              height: 305,
+              width: 305,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: kSecondaryColor,
+              ),
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/image_1_big.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Vegan Salad Bowl\n',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      TextSpan(
+                        text: 'With Red Tomato',
+                        style: TextStyle(
+                          color: kTextColor.withOpacity(.5),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  '\$20',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall
+                      ?.copyWith(color: kPrimaryColor),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 27),
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor.withOpacity(.19),
+                      borderRadius: BorderRadius.circular(27),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Add to Bag',
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        const SizedBox(width: 30),
+                        SvgPicture.asset(
+                          'assets/icons/forward.svg',
+                          height: 11,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: kPrimaryColor.withOpacity(.26),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
